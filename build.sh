dd2cd8f17c4fc446d616c50a3b97b3d7f4b4221a
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-02-19 20:25:02 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-python -m pip install -r requirements.txt
 
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install and build frontend
 cd frontend
 npm install
 npm run build
 cd ..
 
+# Django commands
+cd Backend
 python manage.py collectstatic --noinput
 python manage.py migrate
